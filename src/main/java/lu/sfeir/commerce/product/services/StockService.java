c3e03157d2d7a58dbdae233d4da367b0541b4df2
Alex <a.blanc100@laposte.net>
2023-11-29 09:01:23 +0100

    Add fallback

--- src/main/java/lu/sfeir/commerce/product/services/StockService.java
+++ src/main/java/lu/sfeir/commerce/product/services/StockService.java
@@ -1,16 +1,29 @@
 package lu.sfeir.commerce.product.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import lu.sfeir.commerce.product.dto.StockDto;
 
-@FeignClient(value = "stocks", url = "http://localhost:8087/api")
+@FeignClient(value = "stocks", url = "http://localhost:8087/api", fallback = StockServiceFallback.class)
 public interface StockService {
 	
 	 @RequestMapping(method = RequestMethod.GET, value = "/stocks")
 	 List<StockDto> getStocks();
+	 
+	 
+}
+@Component 
+class StockServiceFallback implements StockService {
+
+	@Override
+	public List<StockDto> getStocks() {
+		return new ArrayList<>();
+	}
+
 }
