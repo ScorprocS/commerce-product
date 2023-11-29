1569d57925552d039865067d1de05b94bf6185f5
Alex <a.blanc100@laposte.net>
2023-11-29 20:08:43 +0100

    Add retry with fallback

--- src/main/java/lu/sfeir/commerce/product/services/StockService.java
+++ src/main/java/lu/sfeir/commerce/product/services/StockService.java
@@ -8,19 +8,25 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import lu.sfeir.commerce.product.dto.StockDto;
-
-@FeignClient(value = "stocks", url = "http://localhost:8087/api", fallback = StockServiceFallback.class)
+@FeignClient(value = "stocks", url = "http://localhost:8087/api")
+//@FeignClient(value = "stocks", url = "http://localhost:8087/api", fallback = StockServiceFallback.class)
 public interface StockService {
+	@Retry(name = "StockService-getStocks", fallbackMethod = "retryFallBack")
+	@RequestMapping(method = RequestMethod.GET, value = "/stocks")
+	List<StockDto> getStocks();
 	
-	 @RequestMapping(method = RequestMethod.GET, value = "/stocks")
-	 List<StockDto> getStocks();
-	 
-	 
-}
-@Component 
-class StockServiceFallback implements StockService {
+	//Could return empty array or last cached request
+	default List<StockDto> retryFallBack(Throwable t) {
+		return new ArrayList<>();
+	}
 
+}
+
+@Component
+class StockServiceFallback implements StockService {
+	//Could return empty array or last cached request
 	@Override
 	public List<StockDto> getStocks() {
 		return new ArrayList<>();
