633528ccfce18c5fb65166bd702b4a46a5b1b6f5
Alex <a.blanc100@laposte.net>
2023-11-30 10:22:55 +0100

    use registry to send request to stocks

--- src/main/java/lu/sfeir/commerce/product/CommerceProductApplication.java
+++ src/main/java/lu/sfeir/commerce/product/CommerceProductApplication.java
@@ -2,11 +2,13 @@ package lu.sfeir.commerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 @EnableFeignClients
 public class CommerceProductApplication {
 
